f2b50b06552a20c84c5cff02c54b9c085459fd61
quangnguyen12 <nguyenbaoquang1@gmail.com>
2022-03-10 21:26:42 +0700

    thêm phần OnDrawGizmosSelected

--- main char/Assets/Code/Combat.cs
+++ main char/Assets/Code/Combat.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class Combat : MonoBehaviour
 {
     public Animator myanin;
     public Transform attackpoint;
     public float attackrange = 0.5f;
+    public LayerMask enenylayer;
     // Update is called once per frame
     void Update()
     {
@@ -19,5 +21,14 @@ public class Combat : MonoBehaviour
     void Attack()
     {
         myanin.SetTrigger("defend");
+        Collider2D [] hitenemies = Physics2D.OverlapCircleAll(attackpoint.position, attackrange, enenylayer);
+        foreach(Collider2D hit in hitenemies)
+        {
+
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackpoint.position, attackrange);
     }
 }
